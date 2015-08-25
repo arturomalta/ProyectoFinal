084caaee4bcf06250a9621f14049f64394f91ad6
arturomalta <arturo.malta@outlook.com>
2015-08-25 18:48:56 -0500

    Initial Commit

---
+++ MyBudget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyBudget
+//
+//  Created by Arturo Malta Vargas on 8/25/15.
+//  Copyright (c) 2015 Ummagumma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyBudget/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyBudget
+//
+//  Created by Arturo Malta Vargas on 8/25/15.
+//  Copyright (c) 2015 Ummagumma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
